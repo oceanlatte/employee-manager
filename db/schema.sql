2b0fad7cbe67175868a843d6d5d077b56143f993
ocean <osciris.veliz@gmail.com>
2022-08-10 17:07:43 -0500

    add role table and mock seed values

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,15 @@
+DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2),
+  department_id INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,13 @@ VALUES
   ('Customer Service');
 
   
+INSERT INTO role (title, salary, department_id) 
+VALUES
+  ('Sales lead', 50000.80, '1'),
+  ('Sales associate', 10000.10, '1'),
+  ('HR Rep', 50000.80, '2'),
+  ('HR Manager', 70000.80, '2'),
+  ('Engineer', 80000.80, '3'),
+  ('Senior Engineer', 100000.80, '3'),
+  ('Customer Service Rep', 10000.80, '4'),
+  ('Customer Service Lead', 40000.80, '4');
